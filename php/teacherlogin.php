a63deb69b882480935fca88eaa585e849efa4878
14mclean <m.mclean1@lancaster.ac.uk>
2022-08-12 18:26:39 +0100

    fix get

--- php/teacherlogin.php
+++ php/teacherlogin.php
@@ -14,8 +14,7 @@
     $response = $conn->query($query);
     
     if(mysqli_num_rows($response) == 0) {
-        $_GET['loginError'] = true;
-        header("Location: ../userlogin.php");
+        header("Location: ../userlogin.php?loginError");
         exit();
     } else {
         echo("Logged In");
