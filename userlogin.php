e954c565bef7b1986819898b9fa733a765f8ca9f
14mclean <m.mclean1@lancaster.ac.uk>
2022-08-13 12:23:05 +0100

    Update userlogin.php

--- userlogin.php
+++ userlogin.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel="stylesheet" href="css/userlogin.css">
     </head>
 
