4941fbfd686fabfbed989582fb96632fe18618d5
14mclean <m.mclean1@lancaster.ac.uk>
2022-08-14 22:53:02 +0100

    Added error messages

--- usersignup.php
+++ usersignup.php
@@ -12,6 +12,26 @@
         <div class="container">
             <h1>Sign Up</h1>
 
+            <div class="loginError">
+                    <?php 
+                        if($_GET["charLengthError"] == "true") {
+                            echo("Password must to be at least 8 characters long");
+                        }
+
+                        if($_GET["numError"] == "true") {
+                            echo("Password must contain at least 1 number");
+                        }
+
+                        if($_GET["uppercaseError"] == "true") {
+                            echo("Password must contain at least 1 uppercase character");
+                        }
+
+                        if($_GET["lowercaseError"] == "true") {
+                            echo("Password must contain at least 1 lowercase character");
+                        }
+                    ?>
+                </div>
+
             <form method="post" action="php/teacherSignup.php"> 
                 <input type="email" placeholder="Email" name="email" id="email"><br>
                 <input type="password" placeholder="Password" name="password" id="passwordObscured"> <img class="eyeIcon" src="assets/openEye.png">
