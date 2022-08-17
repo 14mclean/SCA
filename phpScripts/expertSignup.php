5d2368d72dfcad76104503a7c854e7e4e631caa1
14mclean <60158444+14mclean@users.noreply.github.com>
2022-08-17 12:44:16 +0100

    some php fixes

---
+++ phpScripts/expertSignup.php
@@ -0,0 +1,69 @@
+<?php
+    ini_set('display_errors', 1);
+    require 'connectDB.php';
+    
+    $errors = array();
+    
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        array_push($errors, 'Invalid email');
+    }
+    
+    $errors = [
+        "charLengthError" => strlen($password) < 8, // Password length >= 8
+        "numError" => preg_match('@[0-9]@', $password) < 1, // Password number characters >= 1
+        "uppercaseError" => preg_match('@[A-Z]@', $password) < 1, // Password uppercase characters >= 1
+        "lowercaseError" => preg_match('@[a-z]@', $password) < 1, // Password lowercase characters >= 1
+        "emailTakenError" => false // E-mail must be unique
+    ];
+    
+    if(isError($errors)) {
+        $passHash = hash('sha256', $password);
+        $query = "INSERT INTO Users (email, passwordHash) VALUES ('$email','$passHash');";
+
+        try {
+            $result = $conn->query($query);
+        } catch(Exception $e) {
+            $errors["emailTakenError"] = true;
+            errorOccured($errors);
+        }
+        
+        session_start();
+        $_SESSION["email"] = $email;
+        $_SESSION["loginType"] = "teacher";
+        header("Location: ../webpages/directoryresults");
+        exit();
+    } else {
+        errorOccured($errors);
+    }
+
+    function isError($errors) {
+        foreach ($errors as $key => $value) {
+            if($value == true) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function errorOccured($errors) {
+        $url = "Location: ../webpages/usersignup?";
+
+        foreach ($errors as $key => $value) {
+            if(array_search($key, array_keys($errors)) != 0) {
+                $url .= "&";
+            }
+            $url .= "$key=";
+            if($value) {
+                $url .= "true";
+            } else {
+                $url .= "false";
+            }
+        }
+
+        header($url);
+        exit();
+    }
+?>
--- phpScripts/login.php
+++ phpScripts/login.php
@@ -2,30 +2,31 @@
 
     ini_set("display_errors", 1); // show errors in html (remove after dev)
 
-    require "connectDB.php";
+    require "connectDB.php"; // connect to database
 
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    $passHash = hash("sha256", $password);
+    $passHash = hash("sha256", $_POST["password"]); // hash user inputted password
 
+    
     $statement = $conn->prepare(
-        "SELECT * FROM Users WHERE email = ? AND passwordHash = ?"
+        "SELECT userID,emailVarified,userLevel FROM Users WHERE email = ? AND passwordHash = ?"     // prepare universal statement to get for user fitting GET variables
     );
-    $statement->bind_param("ss", $email, $passHash);
-    $statement->execute();
-    $loginMatch = $statement->num_rows() == 1;
-    
+    $statement->bind_param("ss", $_POST["email"], $passHash); // input get variables to 
+    $statement->execute(); // execute query     
 
-    if($loginMatch) {
-        session_start();
-        $_SESSION["email"] = $email;
-        $_SESSION["userLevel"] = $statement->get_result()->fetch_column(4);
-        $statement->close();
+    if($statement->num_rows() == 1) { // if details match any in login db
+        $result = $statement->get_result();
+        if($result->fetch_column(1) == 1) { // if email has been varified
+            session_start();
+            $_SESSION["userID"] = $result->fetch_column(0); // get userID matching details and add to session
+            $_SESSION["userLevel"] = $result->fetch_column(2); // get user's permissions and add to session
+            $statement->close();
 
-        header("Location: ../webpages/directoryresults");
+            header("Location: ../webpages/directoryresults"); // redirect to directory
+        } else {
+            header("Location: ../webpages/userlogin?loginError=varifiedEmail"); // report non-varified email
+        }
     } else {
-        header("Location: ../webpages/userlogin?loginError=true");
+        header("Location: ../webpages/userlogin?loginError=login"); // report incorrect details
     }
-    
     exit();
 ?>
