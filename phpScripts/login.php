0f1ac1b4b40c33a7b7b9b6d071709e6bf1faa259
14mclean <m.mclean1@lancaster.ac.uk>
2022-08-19 18:18:25 +0100

    debug

--- phpScripts/login.php
+++ phpScripts/login.php
@@ -1,5 +1,4 @@
 <?php
-
     ini_set("display_errors", 1); // show errors in html (remove after dev)
 
     require "connectDB.php"; // connect to database
@@ -12,22 +11,23 @@
     );
     $statement->bind_param("ss", $_POST["email"], $passHash);
     $statement->execute();  
+    print_r($statement->get_result());
 
     if($statement->num_rows() == 1) { // if details match any in login db
         $result = $statement->get_result();
+
         if($result->fetch_column(1) == 1) { // if email has been verified
             session_start();
             $_SESSION["userID"] = $result->fetch_column(0); // get userID matching details and add to session
             $_SESSION["userLevel"] = $result->fetch_column(2); // get user's permissions and add to session
-            $statement->close();
-
+            
             header("Location: ../webpages/directoryresults.php"); // redirect to directory
         } else {
             header("Location: ../webpages/login.php?loginError=verifiedEmail"); // report non-verified email
         }
     } else {
-        print_r($statement->get_result());
-        //header("Location: ../webpages/login.php?loginError=login"); // report incorrect details
+        header("Location: ../webpages/login.php?loginError=login"); // report incorrect details
     }
+    $statement->close();
     exit();
 ?>
