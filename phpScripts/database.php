ee6d7b2c16e49468a4115d91622bb03d44f28d33
14mclean <60158444+14mclean@users.noreply.github.com>
2022-08-22 13:21:28 +0100

    Update database.php

--- phpScripts/database.php
+++ phpScripts/database.php
@@ -43,23 +43,6 @@
             return $result;
         }
 
-        function temp() {
-            $statement = $conn->prepare(
-                "SELECT userID,emailverified,userLevel FROM Users WHERE email = ? AND passwordHash = ?"     
-            ); // prepare universal statement to get for user fitting GET variables
-            $statement->bind_param("ss", $_POST["email"], $passHash);
-            $statement->execute();
-            $statement->store_result();
-
-            $temp = array();
-            for($i = 0; $i < $statement->num_rows(); $i++) {
-                $statement->bind_result($userID, $emailVerified, $level);
-                $statement->fetch();
-                array_push($temp, array($userID, $emailVerified, $level) );
-            }
-            
-        }
-
         private static function referenceArray(array $array): array {
             $result = array();
 
@@ -69,6 +52,7 @@
             }*/
 
             foreach($array as &$ref) {
+                echo($ref);
                 array_push($result, $ref);
             }
 
