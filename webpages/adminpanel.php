58a6d20f1b18ed13db248141be53b0dbb3991369
14mclean <m.mclean1@lancaster.ac.uk>
2022-08-24 22:47:43 +0100

    created admin panel

---
+++ webpages/adminpanel.php
@@ -0,0 +1,30 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION["userLevel"])) {
+        if($_SESSION["userLevel"] != "Admin") {
+            header("Location: login.php");
+            exit();
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>School Citizen Assemblies</title>
+        <link rel="stylesheet" href="../css/adminpanel.css">
+    </head>
+    <body>
+        <section>
+            <h1>Unapproved Experts</h1>
+        </section>
+
+        <section>
+            <h1>Admin Users</h1>
+        <section>
+    </body>
+</html>
