01d106ae6cd752deb7ab52d2677947a8ef2f80d3
14mclean <60158444+14mclean@users.noreply.github.com>
2022-08-29 13:02:59 +0100

    Update adminPanel.js

--- javascript/adminPanel.js
+++ javascript/adminPanel.js
@@ -1,12 +1,14 @@
 // ---------  approve expert ---------
 const approveButtons = document.querySelectorAll("#unapprovedExperts table tbody tr:nth-child(n+1) td:nth-child(4)");
+const unapprovedEmails = document.querySelectorAll("#unapprovedExperts table tbody tr:nth-child(n+1) td:nth-child(1)");
 
-for(const button of approveButtons) {
-    button.addEventListener("click", approveExpert);
+for(var i = 0; i < approveButtons.length; i++) {
+    approveButtons[i].addEventListener("click", approveExpert, false);
+    approveButtons[i].assocEmail = unapprovedEmails[i].textContent;
 }
 
-function approveExpert() {
-    console.log("Approved");
+function approveExpert(event) {
+    console.log("Approved " + event.currentTarget.assocEmail);
 }
 
 
