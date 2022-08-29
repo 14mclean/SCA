39d8d5ac96a60612fe856ee44503cc8e9339d76e
14mclean <60158444+14mclean@users.noreply.github.com>
2022-08-29 12:56:54 +0100

    Update adminPanel.js

--- javascript/adminPanel.js
+++ javascript/adminPanel.js
@@ -1,5 +1,5 @@
 // ---------  approve expert ---------
-const approveButtons = document.querySelector("#unapprovedExperts table tbody tr:nth-child(n+1) td:nth-child(4)");
+const approveButtons = document.querySelectorAll("#unapprovedExperts table tbody tr:nth-child(n+1) td:nth-child(4)");
 
 for(const button of approveButtons) {
     button.addEventListener("click", approveExpert);
