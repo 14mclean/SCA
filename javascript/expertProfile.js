a8527c1e7d1f82c4e8909069345c4b1f9e039b56
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-01 17:53:37 +0100

    Create expertProfile.js

---
+++ javascript/expertProfile.js
@@ -0,0 +1,32 @@
+// validation on expertise
+const expertiseInput = document.querySelector("input[name='expertise']");
+expertiseInput.addEventListener("input", validateExpertiseInput);
+
+function validateExpertiseInput(event) {
+    // not null
+    if(event.target.value == "") {
+        // cannot be empty popup
+        expertiseInput.style.borderColor = "red";
+    }
+}
+
+// validation on organisation?
+const orgInput = document.querySelector("input[name='company']");
+orgInput.addEventListener("input", validateOrgInput);
+
+function validateOrgInput(event) {
+    
+}
+
+// validation on location
+const locationInput = document.querySelector("input[name='location']");
+orgInput.addEventListener("input", validateLocationInput);
+
+function validateLocationInput(event) {
+    $inputText = event.target.value;
+
+    // fits postcode format: 2-4 chars, starts with letter
+    if(inputText.length < 2 || inputText.length > 4 || !inputText[0].match(/[a-z]/i)) {
+        expertiseInput.style.borderColor = "red";
+    }
+}
