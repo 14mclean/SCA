d4d034bae86d227d07fcc11635a8a848dc894123
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-26 22:00:37 +0100

    Update directoryResults.js

--- javascript/directoryResults.js
+++ javascript/directoryResults.js
@@ -102,5 +102,5 @@ async function updateResults() {
     */
     
     const response = await fetch("../phpScripts/getResults.php?"+getFilter);
-    console.log(response);
+    console.log(response.text());
 }
