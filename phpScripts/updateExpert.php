a41e1f139c4be802be4645217d09cd5e35dd496a
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-26 14:34:54 +0100

    Update updateExpert.php

--- phpScripts/updateExpert.php
+++ phpScripts/updateExpert.php
@@ -16,9 +16,6 @@
 
     $db = new Database();
 
-    print_r(gettype($_POST["studentOnline"]));
-    print_r($_POST["studentOnline"]);
-
     $statement = $db->prepareStatement(
         "UPDATE Experts SET expertise=?, organisation=?, teacherAdvice=?, projectWork=?, studentOnline=?, studentF2F=? studentResources=?, location=?, ages=? WHERE userID = ?",
         "ssiiiiisi",
@@ -29,7 +26,7 @@
             intval($_POST["projectWork"]),
             intval($_POST["studentOnline"]),
             intval($_POST["studentF2F"]),
-            intval($_POST["studentResource"]),
+            intval($_POST["studentResources"]),
             $_POST["location"],
             $_POST["ages"],
             $_SESSION["userID"]
