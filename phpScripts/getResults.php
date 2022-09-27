20c3fe619f10ec91c9cd3bcc3ea04d610621b055
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-27 14:32:53 +0100

    Update getResults.php

--- phpScripts/getResults.php
+++ phpScripts/getResults.php
@@ -5,12 +5,34 @@
 
     print_r($_GET);
 
+    /*
+        [adminVerified] => 1 /
+        [teacherAdvice] => 1 /
+        [projectWork] => 1 /
+        [studentOnline] => 1 /
+        [studentF2F] => 1 /
+        [studentResources] => 1 /
+        [ages] => ks1,ks2,ks3,ks4,ks5
+        [orgs] => wwf,nationaltrust,mbs,unicef,oxfam
+        [expertise] => blah /
+    */
+
+    $filter = "";
+
+    foreach ($get as $key => $value) {
+        if($key != "adminVerified" && $value == 1) {
+            $filter .= " AND ".$key."=1";
+        }
+    }
+
     if(isset($_GET["expertise"])) {
         $innerStatementString = "SELECT userID, location, SOUNDEX(expertise) as fuzzyExpertise FROM Experts WHERE adminVerified=1";
         $statementString = "SELECT userID, location, fuzzyExpertise FROM (".$innerStatementString.") AS temp WHERE fuzzyExpertise=SOUNDEX(?)";
     } else {
         $statementString = "SELECT userID, location FROM Experts WHERE adminVerified=1";
     }
+
+    print_r($statementString)
 
     $db = new Database();
 
