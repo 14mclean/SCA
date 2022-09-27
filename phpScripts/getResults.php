76e55d08fd08a94d03853711e4fcbe3335d26d77
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-27 13:46:33 +0100

    Update getResults.php

--- phpScripts/getResults.php
+++ phpScripts/getResults.php
@@ -9,13 +9,13 @@
         $innerStatementString = "SELECT userID, location, SOUNDEX(expertise) as fuzzyExpertise FROM Experts WHERE adminVerified=1";
         $statementString = "SELECT userID, location, fuzzyExpertise FROM ("+$innerStatementString+") AS temp WHERE fuzzyExpertise=SOUNDEX(?)";
     } else {
-        $statementString = "SELECT userID, location FROM Experts WHERE adminVerified=1"
+        $statementString = "SELECT userID, location FROM Experts WHERE adminVerified=1";
     }
 
     $db = new Database();
 
     $statement = $db->prepareStatement(
-        ,
+        $statementString,
         "s",
         array($_GET["expertise"])
     );
