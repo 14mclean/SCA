c1afb620063e13331edcf79e918d4e5c022b0c65
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-28 11:07:43 +0100

    Update getStatus.php

--- phpScripts/getStatus.php
+++ phpScripts/getStatus.php
@@ -1,6 +1,5 @@
 <?php
-    $url = $_GET["url"];
-    $headers = get_headers($url);
+    $headers = get_headers($_GET["url"]);
     $httpStatusCode = substr($headers[0], 9, 3);
     print_r($httpStatusCode);
 ?>
