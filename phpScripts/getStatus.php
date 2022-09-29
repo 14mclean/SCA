90a81ddcb643bf1d8710b7fa8f587f7d15d878bd
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-29 11:42:35 +0100

    Update getStatus.php

--- phpScripts/getStatus.php
+++ phpScripts/getStatus.php
@@ -1,8 +1,19 @@
 <?php
+    $previousContext = stream_context_get_options(stream_context_get_default());
+
+    stream_context_set_default([
+        'http' => [
+            'timeout' => 2
+        ]
+    ]);
+
     @$headers = get_headers($_GET["url"]);
     @$httpStatusCode = substr($headers[0], 9, 3);
     if($httpStatusCode == "") {
         $httpStatusCode = "404";
     }
+
+    stream_context_set_default($previousContext);
+
     print_r($httpStatusCode);
 ?>
