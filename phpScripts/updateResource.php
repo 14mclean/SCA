205aab25b31b96497fbea4ea7bd84160b0cbe132
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-29 17:36:05 +0100

    Update updateResource.php

--- phpScripts/updateResource.php
+++ phpScripts/updateResource.php
@@ -17,23 +17,48 @@
 
     */
 
+    $userID = $_POST["userID"];
+    $newResources = array_slice($_POST, 1, count($_POST), true);
+
     $db = new Database();
 
     $statement = $db->prepareStatement(
         "SELECT resourceID,name,link FROM ExpertResources WHERE userID=?;",
         "i",
-        array($_POST["userID"])
+        array($userID)
     );
     $currentResources = $db->sendQuery($statement, array("resourceID", "name", "link"));
 
-    print_r($currentResources);
-    print_r("<br>");
-    print_r($_POST);
+    foreach ($currentResources as $key => $value) {
+        if(array_key_exists($value["name"], $newResources) && $newResources[$value["name"]] == $value["link"]) {
+            unset($currentResources[$key]);
+            unset($newResources[$value["name"]]);
+        } else {
+            // delete
+            $statement = $db->prepareStatement(
+                "DELETE FROM ExpertResources WHERE resourceID=?;",
+                "i",
+                array($value["resourceID"])
+            );
+            $db->sendQuery($statement, array());
+            
+        }
+    }
+
+    foreach ($newResources as $name => $link) {
+        // insert
+        $statement = $db->prepareStatement(
+            "INSERT INTO ExpertResources (userID,name,link) VALUES (?,?,?);",
+            "i,s,s",
+            array($userID, $name, $link)
+        );
+        $db->sendQuery($statement, array());
+    }
 
     /*
 
     db: Array ( [0] => Array ( [resourceID] => 1 [name] => Exmaple Resource [link] => test.com/exampleresource ) )
-    post: Array ( [userID] => 1 [resourceNames] => [object NodeList] [resourceLinks] => [object NodeList] )
+    post: Array ( [userID] => 1 [Exmaple_Resource] => http://www.google.co.uk )
 
     */
 ?>
