0bbb91e5e847205977886efc2e4676dd80188d79
14mclean <60158444+14mclean@users.noreply.github.com>
2022-09-30 13:04:52 +0100

    Update database.php

--- phpScripts/database.php
+++ phpScripts/database.php
@@ -51,6 +51,7 @@
                 for($i = 0; $i < $query->num_rows; $i++) {
                     $query->fetch();
                     $row = $tempRow;
+                    print_r($row);
                     array_push($result, $row);
                     unset($row);
                 }
