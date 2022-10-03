f9251317191c2adb68b33a74dc62483df184d637
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-03 22:32:51 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,24 +1,30 @@
 <?php
-    declare(strict_types=1);
-    spl_autoload_register(function($class) {require __DIR__ . "/$class.php";});
-    set_error_handler("ErrorHandler::handle_error");
-    set_exception_handler("ErrorHandler::handle_exception");
 
-    header("Content-type: application/json; charset=UTF-8");
+declare(strict_types=1);
+spl_autoload_register(function($class) {require __DIR__ . "/$class.php";}); // load any not imported classes (assuming their classname=filename)
+set_error_handler("ErrorHandler::handle_error"); // set custom error handler
+set_exception_handler("ErrorHandler::handle_exception"); // set custom exception handler
 
-    $url = substr($_SERVER["REQUEST_URI"], 4);
-    $parts = array_slice(explode("/", $url),1);
-    
-    if($parts[0] != "experts") {
+header("Content-type: application/json; charset=UTF-8"); // set response type as json
+
+$url = substr($_SERVER["REQUEST_URI"], 4); // remove "/api" from URL
+$parts = array_slice(explode("/", $url),1);
+$db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3"); // config file for hardcoded data?
+
+switch($parts[0]) {
+    case "experts":
+        $id = $parts[1] ?? null;
+        $gateway = new ExpertGateway($db);
+        $controller = new ExpertController($gateway);
+        $controller->process_request($_SERVER["REQUEST_METHOD"], $id);
+        break;
+    case "users":
+        $id = $parts[1] ?? null;
+        $gateway = new UserGateway($db);
+        $controller = new ExpertController($gateway);
+        $controller->process_request($_SERVER["REQUEST_METHOD"], $id);
+        break;
+    default:
         http_response_code(404);
         exit();
-    }
-
-    $id = $parts[1] ?? null;
-
-    $db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3"); // config file for hardcoded data?
-    
-    $gateway = new ExpertGateway($db);
-    
-    $controller = new ExpertController($gateway);
-    $controller->process_request($_SERVER["REQUEST_METHOD"], $id);
+}
