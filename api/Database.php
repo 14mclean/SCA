d547233753311bd07675bf32612181cc8d115013
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-03 14:43:55 +0100

    Update Database.php

--- api/Database.php
+++ api/Database.php
@@ -1,7 +1,12 @@
 <?php
 
 class Database {
-    public function __contruct(private string $host, private string $name, private string $username, private string $password) {}
+    public function __construct(
+        private string $host,
+        private string $name,
+        private string $username,
+        private string $password) 
+        {}
 
     public function get_connection(): PDO {
         $dsn = "mysql:host={$this->host};dbname={$this->name};charset=utf8";
