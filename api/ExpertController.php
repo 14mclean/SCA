164b4cec56d4449048d8aa114489c0fa3c60c4ce
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-03 19:23:19 +0100

    Update ExpertController.php

--- api/ExpertController.php
+++ api/ExpertController.php
@@ -21,7 +21,7 @@ class ExpertController {
                 echo json_encode($this->gateway->get_all());
                 break;
             case "POST":
-                $data = json_decode(file_get_contents("php://input"), true);
+                $data = (array) json_decode(file_get_contents("php://input"), true);
                 var_dump($data);
                 break;
         }
