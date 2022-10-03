9442568131c1c422878020f77690ff0fe4294fe5
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-03 22:26:52 +0100

    Update UserController.php

--- api/UserController.php
+++ api/UserController.php
@@ -1,11 +1,11 @@
 <?php
 
 class UserController extends Controller {
-    public function __construct(ResourceGateway $gateway) {
+    public function __construct(UserGateway $gateway) {
         parent::__construct($gateway, "User");
     }
 
-    private function get_validation_errors(array $data, bool $is_new = true): array {
+    protected function get_validation_errors(array $data, bool $is_new = true): array {
         $errors = [];
 
         // do field validation
