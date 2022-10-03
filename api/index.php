6e02200977963d07ce3e8375b7a7f767af27881d
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-03 14:11:31 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,5 +2,12 @@
 
     // $_SERVER["REQUEST_URI"] = post-hostname path
     $url = substr($_SERVER["REQUEST_URI"], 4);
-    $parts = explode("/", $url);
-    var_dump($parts);
+    $parts = array_slice(explode("/", $url),1);
+    
+    if($parts[0] != "experts") {
+        http_response_code(404);
+        exit();
+    }
+
+    $id = $parts[1] ?? null;
+    var_dump($id);
