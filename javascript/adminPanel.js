66160cb96245a27f0c6aade170a0850c4fe8d683
14mclean <60158444+14mclean@users.noreply.github.com>
2022-10-05 11:11:31 +0100

    Update adminPanel.js

--- javascript/adminPanel.js
+++ javascript/adminPanel.js
@@ -21,6 +21,7 @@ for(const button in remove_admin_buttons) {
 function approve_expert(event) {
     data = {"adminApproved": 1};
     API.api_request("experts/"+event.currentTarget.id, "PATCH", JSON.stringify(data));
+    location.reload();
 }
 
 
@@ -28,11 +29,13 @@ function approve_expert(event) {
 function remove_admin(event) {
     data = {"userLevel": "Teacher"};
     API.api_request("users/"+event.currentTarget.id, "PATCH", JSON.stringify(data));
+    location.reload();
 }
 
 
 // ----- Block E-Mail -----
 function block_email(event) { // *** TODO ***
     data = {"email": event.currentTarget.email, "date": "?current date?"};
-    API.api_request("blocked/", "PATCH", JSON.stringify(data));
+    API.api_request("blocked/", "POST", JSON.stringify(data));
+    location.reload();
 }
