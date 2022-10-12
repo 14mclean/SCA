11a1a5c5b6b3dc4d6791eb9000d9cc61669eee8f
Mark McLean <mark@the-mcleans.org>
2022-10-12 12:47:42 +0100

    debug

--- javascript/API.js
+++ javascript/API.js
@@ -33,6 +33,7 @@ export default class API {
         fetch(path)
         .then(response => response.json())
         .then(json => {
+            console.log(json)
             result.push(json)
         })
         return result;
