4265d8584e54c8508545e5809219d7d15ddf9ab5
Mark McLean <mark@the-mcleans.org>
2022-10-22 19:34:32 +0100

    included login page

--- about.php
+++ about.php
@@ -13,7 +13,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>School Citizen Assemblies</title>
+        <title>About - SCA</title>
         <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
         <link rel="stylesheet" href="css/about.css">
     </head>
