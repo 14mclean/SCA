495f92d518f3544d52040f1ac9e8a482e133eec7
Mark McLean <mark@the-mcleans.org>
2022-10-23 17:48:47 +0100

    debug

--- api/index.php
+++ api/index.php
@@ -9,6 +9,7 @@ header("Content-type: application/json; charset=UTF-8"); // set response type as
 
 $url = substr($_SERVER["REQUEST_URI"], 5); // remove "/api" from URL
 $parts = explode("/", $url);
+echo($parts);
 $db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3"); // config file for hardcoded data?
 
 switch($parts[0]) {
