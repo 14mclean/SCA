625fe9e09ca03cdd4217a98dd3bd966ba984a117
Mark McLean <mark@the-mcleans.org>
2022-10-23 17:53:26 +0100

    debug

--- api/UserGateway.php
+++ api/UserGateway.php
@@ -8,6 +8,7 @@ class UserGateway implements Gateway{
     }
 
     public function get_all(): array {
+        var_dump($_GET);
         $statement_string = "SELECT * FROM Users";
         if(count($_GET) > 0) {
             $condition_string = " WHERE";
--- api/index.php
+++ api/index.php
@@ -9,7 +9,6 @@ header("Content-type: application/json; charset=UTF-8"); // set response type as
 
 $url = substr($_SERVER["REQUEST_URI"], 5); // remove "/api" from URL
 $parts = preg_split("/[\/?]/", $url);
-var_dump($parts);
 $db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3"); // config file for hardcoded data?
 
 switch($parts[0]) {
