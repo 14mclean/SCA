87fef7cd44a2dc1a071e0f8d78e1803f73da54dc
Mark McLean <mark@the-mcleans.org>
2022-10-23 17:47:50 +0100

    debug

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ set_exception_handler("ErrorHandler::handle_exception"); // set custom exception
 
 header("Content-type: application/json; charset=UTF-8"); // set response type as json
 
-$url = substr($_SERVER["REQUEST_URI"], 4); // remove "/api" from URL
+$url = substr($_SERVER["REQUEST_URI"], 5); // remove "/api" from URL
 echo($url);
 $parts = array_slice(explode("/", $url),1);
 $db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3"); // config file for hardcoded data?
