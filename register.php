2eabdd2c49d582422e9e81fc264d62ced6f5d2d9
Mark McLean <mark@the-mcleans.org>
2022-10-23 11:31:48 +0100

    css

--- register.php
+++ register.php
@@ -17,6 +17,7 @@
         <main>
             <div class="form_container">
                 <h1>Sign Up</h1>
+                <hr width:"50%" />
                 <h2><?php echo $_GET["level"]?></h2>
 
                 <form action="phpScripts/login.php">
