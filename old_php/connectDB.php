004c2e444c8accc146fb7f1849a85d3b0eb23d96
Mark McLean <mark@the-mcleans.org>
2022-10-24 13:56:27 +0100

    remove old

--- old_php/connectDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conn = new mysqli('localhost', 'mwd3iqjaesdr', 'cPanMT3', 'SchoolCitizenAssemblies');
-
-    if($conn->connect_error) {
-        exit($conn->connect_error);
-        // Couldn't connect to server, inncorrect username/passwd?
-    }
-?>
