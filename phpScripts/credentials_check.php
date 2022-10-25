1ab14c9c52d9c3d278a40bbeae7feb7dea252eec
Mark McLean <mark@the-mcleans.org>
2022-10-25 11:28:16 +0100

    fix

--- phpScripts/credentials_check.php
+++ phpScripts/credentials_check.php
@@ -4,13 +4,14 @@ ini_set("display_errors", 1); // show errors in html (remove after dev)
 
 include_once("../api/Database.php");
 
-$passwordHash = hash("sha256", $_POST["password"]);
+$data = (array) json_decode(file_get_contents("php://input"), true);
+$passwordHash = hash("sha256", $data["password"]);
 
 $db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3");
 $connection = $db->get_connection();
 
 $statement = $connection->prepare("SELECT emailVerified FROM Users WHERE email = :email AND passwordHash = :passwordHash");
-$statement->bindValue(":email", $_POST["email"], PDO::PARAM_STR);
+$statement->bindValue(":email", $data["email"], PDO::PARAM_STR);
 $statement->bindValue(":passwordHash", $passwordHash, PDO::PARAM_STR);
 $statement->execute();
 
