e2e302b63a8f543e2712c08121a3099ddf001d76
Mark McLean <mark@the-mcleans.org>
2022-11-18 20:55:44 +0000

    css

--- directory.php
+++ directory.php
@@ -229,10 +229,10 @@
         refinement_div.classList.toggle("collapsed");
 
         const vert_span = refinement_div.firstElementChild.firstElementChild.firstElementChild;
-        if(vert_span.style.transform == "rotate(0deg)") {
-            vert_span.style.transform = "rotate(90deg)";
-        } else {
+        if(vert_span.style.transform == "rotate(90deg)") {
             vert_span.style.transform = "rotate(0deg)";
+        } else {
+            vert_span.style.transform = "rotate(90deg)";
         }
     }
 
