ae69736e99451ce5d93ec70e6b1c64818790fb04
Mark McLean <mark@the-mcleans.org>
2022-11-21 15:40:46 +0000

    css

--- directory.php
+++ directory.php
@@ -19,7 +19,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Directory - SCA</title>
-        <link href="https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel='stylesheet'>
         <link rel="stylesheet" href="css/directory.css">
     </head>
 
@@ -278,7 +278,7 @@
                     case "student_interactions":
                         for(const student_interactions_checkbox of document.querySelectorAll(".refinement#student-interactions input")) {
                             if(student_interactions_checkbox.checked) {
-                                switch student_interactions_checkbox.value {
+                                switch(student_interactions_checkbox.value) {
                                     case "student_online":
                                         student_online = true;
                                     case "student_f2f":
