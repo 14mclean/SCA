1fde6837b54619a8d92870b668fc39c501a971c3
Mark McLean <mark@the-mcleans.org>
2022-11-21 14:45:25 +0000

    css

--- directory.php
+++ directory.php
@@ -196,7 +196,8 @@
             <div id="right">
                 <div id="search-container">
                     <input type="text" placeholder="Search Expertise">
-                    <img src="assets/searchIcon.png">
+                        <img src="assets/searchIcon.png">
+                    </input>
                 </div>
                 
                 <div id="results">
