f8116310f9eafcb2319401c119274634027428bd
Mark McLean <mark@the-mcleans.org>
2022-11-22 11:59:43 +0000

    fix

--- email-validation.php
+++ email-validation.php
@@ -8,35 +8,35 @@ $connection = $db->get_connection();
 $code = $_GET["code"];
 
 // get userID for code
-$statement = $connection->prepare("SELECT * FROM EmailCodes WHERE code = :code");
+$statement = $connection->prepare("SELECT * FROM Email_Code WHERE code = :code");
 $statement->bindValue(":code", $code, PDO::PARAM_STR);
 $statement->execute();
 
 $result = $statement->fetch(PDO::FETCH_ASSOC);
-$userID = $result[0]["userID"] ?? $result["userID"];
+$user_id = $result[0]["user_id"] ?? $result["user_id"];
 
 // get userLevel for userID
-$statement = $connection->prepare("SELECT * FROM Users WHERE userID = :userid");
-$statement->bindValue(":userid", $userID, PDO::PARAM_INT);
+$statement = $connection->prepare("SELECT * FROM Users WHERE user_id = :user_id");
+$statement->bindValue(":user_id", $userID, PDO::PARAM_INT);
 $statement->execute();
 
 $result = $statement->fetch(PDO::FETCH_ASSOC);
-$userLevel = $result[0]["userLevel"] ?? $result["userLevel"];
+$user_level = $result[0]["user_level"] ?? $result["user_level"];
 
 // update user to email verified
-$statement = $connection->prepare("UPDATE Users SET emailVerified=1 WHERE userID=:userid");
-$statement->bindValue(":userid", $userID, PDO::PARAM_INT);
+$statement = $connection->prepare("UPDATE Users SET email_verified=1 WHERE user_id=:user_id");
+$statement->bindValue(":user_id", $userID, PDO::PARAM_INT);
 $statement->execute();
 
 // delete code
-$statement = $connection->prepare("DELETE FROM EmailCodes WHERE code=:code");
+$statement = $connection->prepare("DELETE FROM Email_Code WHERE code=:code");
 $statement->bindValue(":code", $code, PDO::PARAM_STR);
 $statement->execute();
 
 // start session and add userID & userLevel to session
 session_start();
-$_SESSION["user_id"] = $userID;
-$_SESSION["user_level"] = $userLevel;
+$_SESSION["user_id"] = $user_id;
+$_SESSION["user_level"] = $user_level;
 
 ?>
 
