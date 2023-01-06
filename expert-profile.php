3cbcef8ba5a29514ca6a27612d8c0a169cbdb6c1
Mark McLean <mark@the-mcleans.org>
2023-01-06 21:38:03 +0000

    changed home to index

--- expert-profile.php
+++ expert-profile.php
@@ -74,7 +74,7 @@ $expertise = $result;
 
     <body>
         <header>
-            <h1><a href="home.php">School Citizen Assemblies</a></h1>
+            <h1><a href="index.php">School Citizen Assemblies</a></h1>
 
             <nav>
                 <a href="about.php">About Us</a>
