7f2a9913ec3f099cba79898883c40d12e52e25a5
Mark McLean <mark@the-mcleans.org>
2023-01-06 21:57:05 +0000

    fix

--- expert-resources.php
+++ expert-resources.php
@@ -13,7 +13,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>About - SCA</title>
+        <title>Expert Resources - SCA</title>
         <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
         <link rel="stylesheet" href="css/expert-resources.css">
     </head>
