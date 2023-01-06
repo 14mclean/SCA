125989c0d2c3d1d8745218b071386b34b9547ae0
Mark McLean <mark@the-mcleans.org>
2023-01-06 20:27:16 +0000

    updated directory

--- directory.php
+++ directory.php
@@ -25,28 +25,70 @@
 
     <body>
         <header>
-            <h1><a href="home.html">School Citizen Assemblies</a></h1>
+        <h1 id="title-heading"><a href="home.html">School Citizen Assemblies</a></h1>
 
-            <nav>
-                <a href="about.php">About Us</a>
-                <a>Teacher Resources</a>
-                <a href="meet-the-experts.php">Meet The Experts</a>
-            </nav>
+        <nav id="menu">
+                <svg id="close-nav" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" overflow="visible" stroke="#ddd" stroke-width="6" stroke-linecap="round">
+                    <line x2="50" y2="50" />
+                    <line x1="50" y2="50" />
+                 </svg>
 
-            <nav class="subnav">
-                <a>Expert Resources</a>
-                <a href="directory.php">Directory</a>
+                <ul>
+                    <li>
+                        <button class="nav-button" id="about">About Us</button>
+                    </li>
+                    <li>
+                        <button class="nav-button" id="teacher-resources" onclick="show_subnav(this)">Teacher Resources</button>
 
-                <?php
-                if($logged_in) {
-                    if($user_level == "Admin") {
-                        echo('<a>Admin Panel</a>');
+                        <div class="subnav" id="teacher-resources">
+                            <a>Student Resources</a>
+                            <a>Teacher Resources</a>
+                            <a>SCA Toolkit</a>
+                        </div>
+                    </li>
+                    <li>
+                        <button class="nav-button" id="mte" onclick="show_subnav(this)">Meet The Experts</button>
+
+                        <div class="subnav" id="mte">
+                            <a href="meet-the-experts.php">Meet The Experts</a>
+                            <a href="expert-resources.php">Expert Resources</a>
+                            <a href="directory.php">Directory</a>
+                        </div>
+                    </li>
+
+                    <?php
+
+                    if($logged_in) {
+                        echo('
+                            <li>
+                                <button class="nav-button" id="my-account" onclick="show_subnav(this)">My Account</button>
+
+                                <div class="subnav" id="my-account">
+                        ');
+                        if($user_level == "Admin") {
+                            echo('<a>Admin Panel</a>');
+                        }
+                        echo('
+                                <a href="expert-profile.php">Profile</a>
+                                <a href="phpScripts/logout.php">Logout</a>
+                            </div>
+                        </li>
+                        ');
+                    } else {
+                        echo('
+                            <li>
+                                <button onclick="location.href=\'login.html\';" class="nav-button" id="login">Login</button>
+                            </li>
+                        ');
                     }
-                    echo('<a href="phpScripts/logout.php">Logout</a>');
-                    echo('<a href="expert-profile.php">Profile</a>');
-                }
-                ?>
+                    ?>
+                </ul>
             </nav>
+
+            <svg id="burger" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+            </svg>
         </header>
 
         <main>
