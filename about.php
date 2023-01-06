c524d350dbc13eab596a1d5706034b9f77d9a1c7
Mark McLean <mark@the-mcleans.org>
2023-01-06 20:12:48 +0000

    fix

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>About - SCA</title>
         <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
-        <link rel="stylesheet" href="css/new-about.css">
+        <link rel="stylesheet" href="css/about.css">
     </head>
 
     <body>
