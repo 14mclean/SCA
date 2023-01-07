cdde85ec6f92d19661b30cf23743d5528aa54370
Mark McLean <mark@the-mcleans.org>
2023-01-07 11:34:09 +0000

    fix

--- index.php
+++ index.php
@@ -136,18 +136,4 @@
     </body>
 
     <script src="header.js"></script>
-    <script>
-        const menu = document.querySelector("nav");
-        const burger = document.querySelector("#burger")
-
-        burger.addEventListener("click", (event) => {
-            if(menu.style.transform == "translateX(0px)") {
-                menu.style.transform = "translateX(150px)";
-                burger.style.position = "absolute";
-            } else {
-                menu.style.transform = "translateX(0px)";
-                burger.style.position = "fixed";
-            }
-        });
-    </script>
 </html>
