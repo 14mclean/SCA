18ce2ede50554da84a05bc725b3c7e275af2c886
Mark McLean <mark@the-mcleans.org>
2023-01-08 19:58:46 +0000

    test

--- about.php
+++ about.php
@@ -66,7 +66,7 @@
                         echo('
                                 <a href="expert-profile.php">Profile</a>
                                 <a href="phpScripts/logout.php">Logout</a>
-                                <a style="height: 0; color: transparent">My Account></a>
+                                <a style="height: 0; visibility: none">My Account></a>
                             </div>
                         </li>
                         ');
