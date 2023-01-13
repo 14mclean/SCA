ff7cd6952935a5cad4500b445041ce29c76ee69e
Mark McLean <mark@the-mcleans.org>
2023-01-13 22:39:50 +0000

    validation email

--- phpScripts/send_validation_email.php
+++ phpScripts/send_validation_email.php
@@ -23,7 +23,7 @@ do {
     $code = bin2hex(random_bytes(64)); // randomly generate code
 } while(code_exists($connection, $code)); // if code clashes repeat
 
-$message = "
+/*$message = "
 <style>
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
     @import url('https://fonts.googleapis.com/css?family=Raleway');
@@ -94,6 +94,60 @@ $message = "
     </div>
 </div>
 
+";*/
+
+$year = date("Y");
+$message = "
+<html>
+    <head>
+        <title>SCA Account Verification Email</title>
+    </head>
+
+    <body>
+        <table align=\"center\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">
+            <tr>
+                <td bgcolor=\"#401b57\" style=\"padding: 20px;\">
+                    <img src=\"https://www.schoolcitizenassemblies.org/assets/email-logo.png\" alt=\"SCA Logo\" width=\"70%\" style=\"aspect-ratio: 1826/197\"/>
+                </td>
+            </tr>
+
+            <tr>
+                <td bgcolor=\"#eeeeee\"  style=\"padding: 30px 30px 40px 30px;\">
+                    <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                        <tr>
+                            <td>
+                                <h2 style=\"font-family: sans-serif;\">Verify Your Account</h2>
+                                <p style=\"font-family: sans-serif\">Thanks for signing up for our service! To complete your registration, please click the button below to verify your email address.</p>
+
+                                <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                                    <tr>
+                                        <td align=\"center\" bgcolor=\"#6b2c91\" style=\"padding: 12px 18px 12px 18px;\">
+                                            <a href=\"https://schoolcitizenassemblies.org/email-validation.php?code=$code\" style=\"color: #ffffff; text-decoration: none; font: normal normal 100 16px/16px Arial;\">Verify Account</a> <!-- PHP the verification link -->
+                                        </td>
+                                    </tr>
+                                </table>
+
+                                <p style=\"font-family: sans-serif\">If you have any questions or issues, please contact us at <br> <a href=\"mailto:support@schoolcitizenassemblies.org\">support@schoolcitizenassemblies.org</a>.</p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+
+            <tr>
+                <td bgcolor=\"#eeeeee\" style=\"padding: 20px 30px 20px 30px;\">
+                    <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                        <tr>
+                            <td width=\"75%\" style=\"color: #222222; font-family: Arial, sans-serif; font-size: 14px;\">
+                            &copy; School Citizen Assemblies. All rights reserved $year<br/>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
 ";
 
 mail( // send email
