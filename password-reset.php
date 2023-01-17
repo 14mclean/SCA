62f7a0b54611b32f757de3ee2a2f7c3fc9eb73a0
Mark McLean <mark@the-mcleans.org>
2023-01-17 14:08:38 +0000

    test

---
+++ password-reset.php
@@ -0,0 +1,67 @@
+<?php
+
+include_once("api/Database.php");
+
+$db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3");
+$connection = $db->get_connection();
+
+$code = $_GET["code"];
+
+// check if code is still valid
+$statement = $connection->prepare("SELECT user_id,entry_date FROM Password_Reset_Code WHERE code = :code");
+$statement->bindValue(":code", $code);
+$statement->execute();
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+$error = "";
+
+if($result) {
+    $expiry_datetime = DateTime($result["entry_date"]);
+    $expiry_datetime->modify("+1 day");
+
+    $user_id = $result["user_id"];
+
+    if($expiry_datetime > date('Y-m-d H:i:s')) {
+        $error = "Password reset link expired, if you still need "
+    }
+
+    // delete code
+    $statement = $connection->prepare("DELETE FROM Password_Reset_Code WHERE code=:code");
+    $statement->bindValue(":code", $code, PDO::PARAM_STR);
+    $statement->execute();
+} else {
+    $error = "This password reset link does not exist";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Password Reset - SCA</title>
+        <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
+        <link rel="stylesheet" href="css/forgot-password.css">
+    </head>
+
+    <body>
+        <header>
+            <a class="back_arrow" href="javascript:history.back()">🠔</a>
+            <h1><a href="/">School Citizen Assemblies</a></h1>
+        </header>
+
+        <main>
+            <div class="form_container">
+                <h1>Password Reset</h1>
+
+                <p id="descriptor">Enter a new password for your account</p>
+
+                <form action="phpScripts/reset_password.php" method="POST">
+                    <input id="password_input" name="password" type="password" placeholder="Password" autocomplete="new-password" required>
+                    <input id="confirm_password_input" name="password" type="password" placeholder="Confirm Password" autocomplete="new-password" required>
+                    <button type="submit">Reset</button>
+                </form>
+            </div>
+        </main>
+    </body>
+</html>
---
+++ phpScripts/send_password_reset_email.php
@@ -0,0 +1,103 @@
+<?php
+
+ini_set("display_errors", 1); // show errors in html (remove after dev)
+
+$email = $_POST["email"]
+
+include_once("../api/Database.php");
+$db = new Database("localhost", "SchoolCitizenAssemblies", "mwd3iqjaesdr", "cPanMT3");
+$connection = $db->get_connection();
+
+$statement = $connection->prepare("SELECT user_id FROM Users WHERE email = :email");
+$statement->bindValue(":email", $email, PDO::PARAM_STR);
+$statement->execute();
+$result = $statement->fetch(PDO::FETCH_ASSOC);
+$user_id = $result["user_id"];
+
+function code_exists($connection, $code) {
+    $statement = $connection->prepare("SELECT * FROM Password_Reset_Code WHERE code = :code");
+    $statement->bindValue(":code", $code, PDO::PARAM_STR);
+    $statement->execute();
+    $result = $statement->fetch(PDO::FETCH_ASSOC);
+    if($result == false) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+do {
+    $code = bin2hex(random_bytes(64)); // randomly generate code
+} while(code_exists($connection, $code)); // if code clashes repeat
+
+// add code to db
+$statement = $connection->prepare("INSERT INTO `Password_Reset_Code`(`user_id`, `code`, `entry_date`) VALUES (:user_id, :code, :entry_date)");
+$statement->bindValue(":user_id", $user_id);
+$statement->bindValue(":code", $code);
+$statement->bindValue(":entry_date", date('Y-m-d H:i:s'));
+$statement->execute();
+
+// send create email with code link to password reset
+$year = date("Y");
+$message = "
+<html>
+<head>
+    <title>SCA Password Reset</title>
+</head>
+
+<body>
+    <table align=\"center\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\">
+        <tr>
+            <td bgcolor=\"#401b57\" style=\"padding: 20px;\">
+                <img src=\"https://www.schoolcitizenassemblies.org/assets/email-logo.png\" alt=\"SCA Logo\" width=\"70%\" style=\"aspect-ratio: 1826/197\"/>
+            </td>
+        </tr>
+
+        <tr>
+            <td bgcolor=\"#eeeeee\"  style=\"padding: 30px 30px 40px 30px;\">
+                <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                    <tr>
+                        <td>
+                            <h2 style=\"font-family: sans-serif;\">Reset Your Password</h2>
+                            <p style=\"font-family: sans-serif\">We received a request to reset the password for your account. If you did not make this request you can ignore this email.</p>
+
+                            <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                                <tr>
+                                    <td align=\"center\" bgcolor=\"#6b2c91\" style=\"padding: 12px 18px 12px 18px;\">
+                                        <a href=\"https://schoolcitizenassemblies.org/password-reset.php?code=$code\" style=\"color: #ffffff; text-decoration: none; font: normal normal 100 16px/16px Arial;\">Reset Password</a> <!-- PHP the verification link -->
+                                    </td>
+                                </tr>
+                            </table>
+
+                            <p style=\"font-family: sans-serif\">If you have any questions or issues, please contact us at <br> <a href=\"mailto:support@schoolcitizenassemblies.org\">support@schoolcitizenassemblies.org</a>.</p>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td bgcolor=\"#eeeeee\" style=\"padding: 20px 30px 20px 30px;\">
+                <table cellpadding=\"0\" cellspacing=\"0\" width=\"100%\">
+                    <tr>
+                        <td width=\"75%\" style=\"color: #222222; font-family: Arial, sans-serif; font-size: 14px;\">
+                        &copy; School Citizen Assemblies. All rights reserved $year<br/>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+";
+
+mail(
+    $email,
+    "Password Reset",
+    $message,
+    "From: SCA <sca@schoolcitizenassemblies.org>\r\nMIME-Version: 1.0\r\nContent-Type: text/html; charset=UTF-8\r\n"
+)
+
+header("/");
+exit();
