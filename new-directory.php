55c216a0794da938ad554ad4c8a6bc0b4a988134
Mark McLean <mark@the-mcleans.org>
2023-01-31 13:43:16 +0000

    fix

--- new-directory.php
+++ new-directory.php
@@ -281,17 +281,39 @@
                     if($response["status"] == 200) {
                         return [
                             floatval($response["result"]["latitude"]),
-                            floatval((float) $response["result"]["longitude"])
+                            floatval($response["result"]["longitude"])
                         ];
                     } else {
                         return false;
                     }
                 }
 
+                function location_distance(array $location1, array $location2): float {
+                    define("R", 6371e3); // earth radius (m)
+                    $latitude1_radians = $location1[0] * M_PI / 180;
+                    $latitude2_radians = $location2[0] * M_PI / 180;
+                    $latitude_delta = ($location2[0] - $location1[0]) * M_PI / 180;
+                    $longitude_delta = ($location2[1] - $location1[1]) * M_PI / 180;
+                    $haversine_a = 
+                        sin($latitude_delta / 2) *
+                        sin($latitude_delta / 2) +
+                        cos($latitude1_radians) *
+                        cos($latitude2_radians) *
+                        sin($longitude_delta / 2) *
+                        sin($longitude_delta / 2);
+                    $haversine_c = 2 * atan2(
+                        sqrt($haversine_a),
+                        sqrt(1 - $haversine_a)
+                    );
+                    $distance = R * $haversine_c;
+                    return $distance;
+                }
+
                 $current_coords = outcode_to_coords($postcode, "postcodes");
                 foreach($result as $expert) {
-                    [$lat, $long] = outcode_to_coords($expert["location"]);
-                    
+                    $expert_coords = outcode_to_coords($expert["location"]);
+                    $distance = location_distance($current_coords, $expert_coords);
+                    print_r($distance);
                 }
 
                 /*
