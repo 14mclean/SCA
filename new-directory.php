7d80d62f8432ef1f92f96ecda9afbfb7b33623d9
Mark McLean <mark@the-mcleans.org>
2023-02-02 12:10:06 +0000

    fix

--- new-directory.php
+++ new-directory.php
@@ -404,6 +404,7 @@
         </script>
         <script src="https://cdn.jsdelivr.net/npm/fuzzysort@2.0.4/fuzzysort.min.js"></script>
         <script src="javascript/header.js"></script>
+        <script src="javascript/debounce.js"></script>
         <script src="javascript/new-directory.js"></script>
     </body>
 </html>
