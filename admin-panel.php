bac307c7ef5c4110e01f604d14f2b171691481a1
Mark McLean <mark@the-mcleans.org>
2023-02-16 14:48:27 +0000

    fix

--- admin-panel.php
+++ admin-panel.php
@@ -215,17 +215,6 @@
                             <th class="table-subheading">Demote</th>
                         </tr>
 
-                        <tr id="0">
-                            <td>email</td>
-                            <td>
-                                <button class="table-button remove-admin">
-                                    <svg width="24px" height="24px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M16 2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-2.939 12.789L10 11.729l-3.061 3.06-1.729-1.728L8.271 10l-3.06-3.061L6.94 5.21 10 8.271l3.059-3.061 1.729 1.729L11.729 10l3.06 3.061-1.728 1.728z", fill="#ff0000"/>
-                                    </svg>
-                                </button>
-                            </td>
-                        </tr>
-
                         <?php
                             $statement = $connection->prepare("SELECT user_id, email from User WHERE user_level='Admin'");
                             $statement->execute();
