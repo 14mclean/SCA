466817f41fd9109602a63352784faf9089b8795b
Mark McLean <mark@the-mcleans.org>
2023-02-22 13:02:14 +0000

    wrapper for PDOStatement class to add logging

--- api/Database.php
+++ api/Database.php
@@ -16,6 +16,29 @@ class Database {
             $this->password,
             [PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_STRINGIFY_FETCHES => false]
         );
+        $pdo->setAttribute(PDO::ATTR_STATEMENT_CLASS, [LoggedPDOStatement::class, [$pdo]]);
         return $pdo;
+    }
+}
+
+class LoggedPDOStatement extends PDOStatement
+{
+    protected $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function execute(?array $params = null): bool {
+        // time and execute the query
+        $start_time = microtime(true);
+        $result = parent::execute($params);
+        $end_time = microtime(true);
+
+        // Log the query to a table
+        $stmt = $this->pdo->prepare("INSERT INTO `Query_Log`(`timestamp`, `query_body`, `query_execute_length`) VALUES (?, ?, ?)");
+        $stmt->execute([date('Y-m-d H:i:s'), $this->queryString, $end_time - $start_time]);
+
+        return $result;
     }
 }
