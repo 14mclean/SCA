f86a478ddcf8be40eda04a04951d68b568152382
Mark McLean <mark@the-mcleans.org>
2023-06-01 15:58:10 +0100

    new about content

--- about.php
+++ about.php
@@ -94,10 +94,25 @@
 
         <main>
             <h2>About Us</h2>
-            <p id="description">The School Citizen Assemblies (SCA) approach was developed initially by Dr Chris McLean (Alliance Manchester Business School: AMBS) with the support of colleagues from around the University of Manchester. This includes Professor Jonatan Pinkse (AMBS); Dr Jennifer O’Brien (Geography) and Dr Louis Major (Manchester Institute of Education). There have also been many others involved in the design, development and application of the SCA and we wish to thank them for all of their contributions, ideas and reflections.</p>
+            <p id="description">T
+                he School Citizen Assemblies (SCA) approach was developed initially by Dr Chris McLean (Alliance Manchester Business School: AMBS) with the support of colleagues from around the University of Manchester. This includes Professor Jonatan Pinkse (AMBS); Dr Jennifer O'Brien (Geography) and Dr Louis Major (Manchester Institute of Education). There have also been many others involved in the design, development and application of the SCA and we wish to thank them for all of their contributions, ideas and reflections.
+            </p>
         
             <h3 class="name-title">Dr Chris McLean</h3>
-            <p class="person-description">Chris founded the SCA. Along the journey of designing, developing and implementing the SCA, she has been supported by a wide range of people. She is an academic and educationalist based within the Alliance Manchester Business School (AMBS), at the University of Manchester.<br>Chris has a keen interest in curriculum design, assessment, civic engagement and sustainable education. She is also academic lead of the University of Manchester School Governors Initiative.  Through her work she seeks to promote the incredible and inspirational work of schools and teachers and also help them to share and develop innovative teaching and learning approaches. She is also lead of the RSA Innovative Education Network and has been working with primary and secondary schools for many years in the area of education and organisational change.</p>
+            <p class="person-description">
+                Chris founded the SCA. Along the journey of designing, developing and implementing the SCA, she has been supported by a wide range of people. She is an academic and educationalist based within the Alliance Manchester Business School (AMBS), at the University of Manchester.<br><br>Chris has a keen interest in curriculum design, assessment, civic engagement and sustainable education. She is also academic lead of the University of Manchester School Governors Initiative.  Through her work she seeks to promote the incredible and inspirational work of schools and teachers and also help them to share and develop innovative teaching and learning approaches. She is also lead of the RSA Innovative Education Network and has been working with primary and secondary schools for many years in the area of education and organisational change.
+            </p>
+        
+            <section id="manchester-team">
+                <h2>The University of Manchester Team<h2>
+
+                <div id="team-member">
+                    <h3 class="name-title">Dr Jennifer O'Brien</h3>
+                    <p class="person-description">
+                        Dr. Jennifer O'Brien (PFHEA) is Academic Lead for Sustainability Teaching and Learning at the University of Manchester, UK, and an Inaugural Fellow of the Manchester Institute of Teaching and Learning.  Jen is the Education Lead for Sustainable Futures and directs the University Living Lab which links applied research needed by organisations with students who can undertake it for their assessment to effect change. Working with Education for Sustainable Development particularly through the United Nation's Sustainable Development Goals, Jen aims to inspire and equip learners to ethically address challenges of sustainability, inequality and social justice to effect positive change.
+                    </p>
+                </div>
+            </section>
         </main>
 
         <footer>
