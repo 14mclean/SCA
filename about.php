a6f93b5b307ff23d709ba131a114cd9820320470
Mark McLean <mark@the-mcleans.org>
2023-06-02 10:44:07 +0100

    fixing bad reqs

--- about.php
+++ about.php
@@ -98,38 +98,35 @@
                 The School Citizen Assemblies (SCA) approach was developed initially by Dr Chris McLean (Alliance Manchester Business School: AMBS) with the support of colleagues from around the University of Manchester. This includes Professor Jonatan Pinkse (AMBS); Dr Jennifer O'Brien (Geography) and Dr Louis Major (Manchester Institute of Education). There have also been many others involved in the design, development and application of the SCA and we wish to thank them for all of their contributions, ideas and reflections.
             </p>
         
-            <h3 class="name-title">Dr Chris McLean</h3>
-            <p class="person-description">
-                Chris founded the SCA. Along the journey of designing, developing and implementing the SCA, she has been supported by a wide range of people. She is an academic and educationalist based within the Alliance Manchester Business School (AMBS), at the University of Manchester.<br><br>Chris has a keen interest in curriculum design, assessment, civic engagement and sustainable education. She is also academic lead of the University of Manchester School Governors Initiative.  Through her work she seeks to promote the incredible and inspirational work of schools and teachers and also help them to share and develop innovative teaching and learning approaches. She is also lead of the RSA Innovative Education Network and has been working with primary and secondary schools for many years in the area of education and organisational change.
-            </p>
-        
-            <section id="manchester-team">
-                <h2>The University of Manchester Team</h2>
+            <div class="team-member">
+                <h3 class="name-title">Dr Chris McLean</h3>
+                <p class="person-description">
+                    Chris founded the SCA. Along the journey of designing, developing and implementing the SCA, she has been supported by a wide range of people. She is an academic and educationalist based within the Alliance Manchester Business School (AMBS), at the University of Manchester.<br><br>Chris has a keen interest in curriculum design, assessment, civic engagement and sustainable education. She is also academic lead of the University of Manchester School Governors Initiative.  Through her work she seeks to promote the incredible and inspirational work of schools and teachers and also help them to share and develop innovative teaching and learning approaches. She is also lead of the RSA Innovative Education Network and has been working with primary and secondary schools for many years in the area of education and organisational change.
+                </p>
 
-                <div class="team-member">
-                    <h3 class="name-title">Dr Jennifer O'Brien</h3>
-                    <img src="assets/JOBrien.png">
-                    <p class="person-description">
-                        Dr. Jennifer O'Brien (PFHEA) is Academic Lead for Sustainability Teaching and Learning at the University of Manchester, UK, and an Inaugural Fellow of the Manchester Institute of Teaching and Learning.  Jen is the Education Lead for Sustainable Futures and directs the University Living Lab which links applied research needed by organisations with students who can undertake it for their assessment to effect change. Working with Education for Sustainable Development particularly through the United Nation's Sustainable Development Goals, Jen aims to inspire and equip learners to ethically address challenges of sustainability, inequality and social justice to effect positive change.
-                    </p>
-                </div>
+            <div class="team-member">
+                <h3 class="name-title">Dr Jennifer O'Brien</h3>
+                <img src="assets/JOBrien.png">
+                <p class="person-description">
+                    Dr. Jennifer O'Brien (PFHEA) is Academic Lead for Sustainability Teaching and Learning at the University of Manchester, UK, and an Inaugural Fellow of the Manchester Institute of Teaching and Learning.  Jen is the Education Lead for Sustainable Futures and directs the University Living Lab which links applied research needed by organisations with students who can undertake it for their assessment to effect change. Working with Education for Sustainable Development particularly through the United Nation's Sustainable Development Goals, Jen aims to inspire and equip learners to ethically address challenges of sustainability, inequality and social justice to effect positive change.
+                </p>
+            </div>
 
-                <div class="team-member">
-                    <h3 class="name-title">Professor Jonatan Pinkse</h3>
-                    <img src="assets/JPinkse.png">
-                    <p class="person-description">
-                        Jonatan is a Professor of strategy, innovation, and entrepreneurship at the Manchester Institute of Innovation Research (MIoIR), Alliance Manchester Business School, The University of Manchester. His research interests focus on corporate sustainability, business model innovation, social entrepreneurship, cross-sector partnerships, and the sharing economy. In his research, Jonatan analyses how firms make strategic decisions to adapt to a more sustainable economy and deal with the ensuing tensions between issues and actors. He also investigates barriers to firm adoption of disruptive technologies from cognitive, organizational, and institutional perspectives. Before moving to Manchester, he held positions at the Universiteit van Amsterdam and Grenoble Ecole de Management. Jonatan has authored more than 50 scholarly and practitioner articles in a variety of journals.
-                    </p>
-                </div>
+            <div class="team-member">
+                <h3 class="name-title">Professor Jonatan Pinkse</h3>
+                <img src="assets/JPinkse.png">
+                <p class="person-description">
+                    Jonatan is a Professor of strategy, innovation, and entrepreneurship at the Manchester Institute of Innovation Research (MIoIR), Alliance Manchester Business School, The University of Manchester. His research interests focus on corporate sustainability, business model innovation, social entrepreneurship, cross-sector partnerships, and the sharing economy. In his research, Jonatan analyses how firms make strategic decisions to adapt to a more sustainable economy and deal with the ensuing tensions between issues and actors. He also investigates barriers to firm adoption of disruptive technologies from cognitive, organizational, and institutional perspectives. Before moving to Manchester, he held positions at the Universiteit van Amsterdam and Grenoble Ecole de Management. Jonatan has authored more than 50 scholarly and practitioner articles in a variety of journals.
+                </p>
+            </div>
 
-                <div class="team-member">
-                    <h3 class="name-title">Dr Louis Major</h3>
-                    <img src="assets/LMajor.png">
-                    <p class="person-description">
-                        Louis Major is a Senior Lecturer in Digital Education at the Manchester Institute of Education (MIE). His research primarily focuses on digital technology’s role in the future of education, in particular, how this can help to address educational disadvantage and support effective dialogue and communication. He co-leads the Digital Technology, Communication and Education Research and Scholarship group at the University of Manchester. He has also been an Editor of the British Journal of Educational Technology (BJET) since 2020.
-                    </p>
-                </div>
-            </section>
+            <div class="team-member">
+                <h3 class="name-title">Dr Louis Major</h3>
+                <img src="assets/LMajor.png">
+                <p class="person-description">
+                    Louis Major is a Senior Lecturer in Digital Education at the Manchester Institute of Education (MIE). His research primarily focuses on digital technology’s role in the future of education, in particular, how this can help to address educational disadvantage and support effective dialogue and communication. He co-leads the Digital Technology, Communication and Education Research and Scholarship group at the University of Manchester. He has also been an Editor of the British Journal of Educational Technology (BJET) since 2020.
+                </p>
+            </div>
         </main>
 
         <footer>
