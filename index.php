9c14128338e49c36f655feee30c155a43d61ee8d
Mark McLean <mark@the-mcleans.org>
2023-10-30 20:22:18 +0000

    Fit image better

--- index.php
+++ index.php
@@ -96,7 +96,7 @@
             <div class="first-section">
                 <!--<h1>School Citizen Assemblies</h1>-->
 
-                <img src="assets/homepage.png">
+                <img src="assets/homepage.png" alt="SCA title and motto">
 
                 <!--<h2><i>Assembling and empowering young people, experts and stakeholders to tackle real world challanges</i></h2>-->
             </div>
